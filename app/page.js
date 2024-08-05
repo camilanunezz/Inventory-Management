38efa7281daff372adf179b90fd733a46211fb9b
camilanunezz <djs8zw@virginia.edu>
2024-08-05 14:44:18 -0400

    Implemented main components
    -basic structure
    -state management
    -inventory fetching (updateInventory function)
    -add and remove items functions
    -modal control functions

--- app/page.js
+++ app/page.js
@@ -1,5 +1,82 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { Box, Stack, Typography, Button, Modal, TextField } from '@mui/material'
+import { firestore } from '@/firebase'
 import Image from "next/image";
+import { collection, doc ,getDocs, query , setDoc, deleteDoc, getDoc,} from'firebase/firestore'
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'white',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 3,
+}
 
 export default function Home() {
-  return <>Hello World</>
+  //state variables
+  const [inventory, setInventory] = useState([])
+  const [open, setOpen] = useState(false)
+  const [itemName, setItemName] = useState('')
+
+  //3 helper functions
+  //1 function to fetch inventory data from Firestore
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+    docs.forEach((doc) => {
+      inventoryList.push({ 
+        name: doc.id, 
+        ...doc.data() })
+    })
+    setInventory(inventoryList)
+  }
+  useEffect(() => {
+    updateInventory()
+  }, [])
+
+  //2
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data()
+      await setDoc(docRef, { quantity: quantity + 1 })
+    } else {
+      await setDoc(docRef, { quantity: 1 })
+    }
+    await updateInventory()
+  }
+  
+  //3
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data()
+      if (quantity === 1) {
+        await deleteDoc(docRef)
+      } else {
+        await setDoc(docRef, { quantity: quantity - 1 })
+      }
+    await updateInventory()
+  }
+
+  //modal control functions
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  return 
+  <Box>
+  <Typography variant="h1">Inventory Management</Typography>
+  </Box>
 }
